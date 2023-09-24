fbb4422b1ade15e0351aab7fc2e19426f5161bb2
Evgeni Doudar <60257081+doudar41@users.noreply.github.com>
2023-09-24 14:30:33 +0200

    Update AudioVolume.cs

--- AudioVolume.cs
+++ AudioVolume.cs
@@ -2,143 +2,50 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
-using FMODUnity;
-
-[System.Serializable]
-public struct AudioVolumeProperties
-{
-    public AudioVolume vol;
-    public float volCrossVolume;
-    
-}
 
 public class AudioVolume : MonoBehaviour
 {
-    bool enter1, enter2, exit1, exit2 = false;
-    public bool inside = false;
+
     public UnityEvent enterAudioVolume;
     public UnityEvent exitAudioVolume;
 
-    [Range(0,1)]
-    public float thisVolumeInitLoudness = 1;
-    public float thisVolumeCurrentVolume = 0;
-    
-    [SerializeField]
-    private AnimationCurve FadeIn, FadeOut;
+    bool enter1, enter2, exit1, exit2 = false;
 
-    public SoundSource[] emitters;
-    public AudioVolumeProperties[] otherAudioVolumes;
-
-
-    private void Start()
+    public void CollisionOneEnter()
     {
-        thisVolumeCurrentVolume = thisVolumeInitLoudness;
-    }
-
-    public void E1Enter() 
-    {
-        enter1 = true; 
+        enter1 = true;
         exit1 = false;
-       //if (enter2) print("between colliders");
     }
-    public void E2Enter() 
-    {   enter2 = true;  
-        exit2 = false;
-       // if (enter1) print("between colliders");
-
-    }
-    public void E1Exit()
+    public void CollisionTwoEnter()
     {
-        exit1 = true; 
+        enter2 = true;
+        exit2 = false;
+    }
+    public void CollisionOneExit()
+    {
+        exit1 = true;
         enter1 = false;
         if (!enter2)
         {
-            if (exit2) { print("outside colliders"); exitAudioVolume.Invoke(); inside = false; exit2 = false; }
+            if (exit2) {  exitAudioVolume.Invoke(); exit2 = false; }
         }
     }
-    
-    public void E2Exit() { 
-        exit2 = true; 
+
+    public void CollisionTwoExit()
+    {
+        exit2 = true;
         enter2 = false;
-        if (!enter1) { if (exit1) { print("inside colliders"); inside = true;  enterAudioVolume.Invoke(); exit1 = false; } }
-    }
-/*    private void Update()
-    {
-        ChangeVolumeOfChildrenSounds(thisVolumeCurrentVolume);
-    }*/
-
-
-    public void PlaySoundSources()
-    {
-        foreach (SoundSource x in emitters)
-        {
-            x.Play(this, FadeIn);
-        }
+        if (!enter1) { if (exit1) { enterAudioVolume.Invoke(); exit1 = false; } }
     }
 
-    public void StopSoundSources()
+
+    public void  EnterVolume()
     {
-        foreach (SoundSource x in emitters)
-        {
-            x.Stop(this, FadeOut);
-        }
+        Debug.Log("enter volume");
     }
 
-    public void GradChangingLoudness(float volume)
+    public void ExitVolume()
     {
-        if (emitters == null) return;
-        bool isMore = true;
-        isMore = CheckVolumeDifference(volume);
-       foreach (SoundSource x in emitters)
-        {
-            if (isMore) x.ChangeVolumePlus(FadeIn,volume);
-            else x.ChangeVolumeMinus(FadeOut, volume);
-        }
+        Debug.Log("exit  volume");
     }
-
-    public void OnPlayerInsideVolume()
-    {
-        if (otherAudioVolumes == null) return;
-        foreach (AudioVolumeProperties x in otherAudioVolumes)
-        {
-            x.vol.thisVolumeCurrentVolume = x.volCrossVolume;
-            x.vol.GradChangingLoudness(x.vol.thisVolumeCurrentVolume);
-        }
-    }
-    public void OnPlayerOutsideVolume()
-    {
-        if (otherAudioVolumes == null) return;
-        foreach (AudioVolumeProperties x in otherAudioVolumes)
-        {
-            x.vol.thisVolumeCurrentVolume = x.vol.thisVolumeInitLoudness;
-            x.vol.GradChangingLoudness(x.vol.thisVolumeCurrentVolume);
-        }
-    }
-
-    public void ApplyGlobalEffect(float effectAmount)
-    {
-        FMODUnity.RuntimeManager.StudioSystem.setParameterByName("ReverbPrivate", effectAmount);
-    }
-
-    public void RemoveGlobalEffect()
-    {
-        FMODUnity.RuntimeManager.StudioSystem.setParameterByName("ReverbPrivate", 0);
-    }
-
-    bool CheckVolumeDifference(float input)
-    {
-
-        if (input > emitters[0].GetSourceVolume()) return true;
-        else return false;
-        
-    }
-
-    public void LowPass(float amount)
-    {
-        foreach (SoundSource x in emitters)
-        {
-            x.ChangeLFP(amount);
-        }
-    }
-
 }
